02859a68abab525fb728a9d4152fe47bbcd09289
SHOKI INOKA <is091160@gmail.com>
2022-05-19 01:14:31 +0900

    Add minimods
    added mini mods.

---
+++ miniMod/miniHello.go
@@ -0,0 +1,10 @@
+package sub
+
+import (
+	"fmt"
+)
+
+func NormalHello() (string, error) {
+
+	return fmt.Sprintf("Hello."), nil
+}
---
+++ subMod/beta/normalHello.go
@@ -0,0 +1,10 @@
+package beta
+
+import (
+	"fmt"
+)
+
+func NormalHello() (string, error) {
+
+	return fmt.Sprintf("Hello."), nil
+}
